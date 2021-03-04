227a1e1515fcf9d27aa0abec3a58bb6218ac94e0
Diego Garcia <diego.garcia@xing.com>
2021-03-04 19:09:22 +0100

    show FP and FN

--- scripts/nlp_disaster_tweets.py
+++ scripts/nlp_disaster_tweets.py
@@ -35,52 +35,137 @@ train_df.keyword.value_counts(dropna=False,normalize=True)
 # %%
 train_df.target.value_counts(dropna=False,normalize=True)
 
-# %%
-import nltk  
-
 # %% [markdown]
-# ## remove stop words
+# ## Preprocessing
 
 # %%
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import RegexpTokenizer, TweetTokenizer
 from nltk.stem.wordnet import WordNetLemmatizer 
 import re
 lem = WordNetLemmatizer()
+tokenizer = TweetTokenizer()
 
 
 # %%
-def preprocess_sentence(sentence):
+def preprocess_sentence(sentence: str) -> list:
     cleanr = re.compile('<.*?>')
     cleantext = re.sub(cleanr, '', sentence)
     regex = re.compile('([^\s\w]|_)+')
     cleantext = regex.sub('', cleantext).lower()
-    tokens = word_tokenize(cleantext)
+    tokens = tokenizer.tokenize(cleantext)
     tokens_normalized = [word.lower() for word in tokens if len(word)>2 and word.lower() not in stopwords.words('english')]
     tokens_lemmatized = [lem.lemmatize(word) for word in tokens_normalized]
     return tokens_lemmatized
 
 
 # %%
-train_df["tokens"] = train_df["text"].map(lambda x: preprocess_sentence(x))
+def preprocess_df(df: pd.DataFrame ) -> pd.DataFrame:
+    df["tokens"] = df["text"].map(lambda x: preprocess_sentence(x))
+    df["features"] = df.tokens.apply(lambda x: " ".join(x))
+    return df
+
 
 # %%
-cv
+train_df = preprocess_df(train_df)
 
 # %%
 terms_frequency = pd.DataFrame({"word": [item for sublist in np.array(train_df.tokens).tolist() for item in sublist ]})
 
 # %%
-corpus = terms_frequency.drop_duplicates().word.tolist()
+terms_frequency.value_counts().head(50)
 
-# %%
-model = word2vec.Word2Vec(train_df.tokens, size=100, window=20, min_count=2, workers=4)
+# %% [markdown]
+# # Text to features
+
+# %% [markdown]
+# ## Bag of Words
 
 # %%
 from sklearn import feature_extraction
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 # %%
-vectorizer = feature_extraction.text.CountVectorizer(max_features=10000, ngram_range=(1,2))
+#vectorizer = feature_extraction.text.CountVectorizer(max_features=10000, ngram_range=(1,3))
+vectorizer = feature_extraction.text.TfidfVectorizer()
+
+# %%
+corpus = train_df.features.to_list()
+vectorizer_fit = vectorizer.fit(corpus)
+X = vectorizer_fit.transform(train_df.features.to_list()) 
+
+# %%
+y = train_df["target"].to_list()
+
+# %%
+X_train_df, X_test_df, y_train, y_test = train_test_split(train_df, y, test_size=0.2, random_state=0)
+
+# %%
+X_train = X[X_train_df.index]
+X_test = X[X_test_df.index]
+
+# %% tags=[]
+classifier = RandomForestClassifier(n_estimators=1000, random_state=0)
+classifier.fit(X_train, y_train) 
+
+# %%
+y_pred = classifier.predict(X_test)
+
+# %%
+print(classification_report(y_test,y_pred))
+print(accuracy_score(y_test, y_pred))
+
+# %%
+from sklearn.metrics import plot_confusion_matrix
+import matplotlib.pyplot as plt
+
+# %%
+plot_confusion_matrix(classifier, X_test, y_test,
+                                 display_labels=["0", "1"],
+                                 cmap=plt.cm.Blues)
+
+# %%
+X_test_df.loc[:,"prediction"] = y_pred
+
+# %%
+X_test_df[X_test_df["target"] != X_test_df["prediction"]].head(50)
+
+# %% [markdown]
+# # Prediction
+
+# %%
+test_df = preprocess_df(test_df)
+
+# %%
+X_test = vectorizer_fit.transform(test_df.features.to_list()) 
+y_test = classifier.predict(X_test)
+#test_df['target'] = 
+
+# %%
+y_test
+
+# %%
+test_df["target"] = y_test
+
+# %% [markdown]
+# # Submission
+
+# %%
+sample_submission
+
+# %%
+submission = test_df[["id","target"]]
+
+# %%
+#submission.to_csv("submission.csv",index=False)
+
+# %%
+# #!kaggle competitions submit -f notebooks/submission.csv -m "BoW and TIDF" nlp-getting-started
+
+# %%
+# #!kaggle competitions submissions nlp-getting-started
 
 # %%
